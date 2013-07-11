4b712872830693feabac0fc3dfd87e031be31536
Andrey Platonov <a.platonov@office.ngs.ru>
2013-07-11 13:37:30 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name='zfs-mon',
+    version='1.0',
+    description='ZFS real-time cache activity monitor',
+    author='Andrey Platonov',
+    author_email='poluandrey@gmail.com',
+    url='https://github.com/tears-of-noobs/zfs-mon.git',
+    scripts=['zfs-mon']
+)
